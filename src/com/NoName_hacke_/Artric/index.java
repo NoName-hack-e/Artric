c04dd7723eb5652791ce2747e1e122c916f33ad4
NoName-hack-e <NoName_hacke@outlook.com>
2021-09-23 21:34:46 +0600

    Suusy

--- src/com/NoName_hacke_/Artric/index.java
+++ src/com/NoName_hacke_/Artric/index.java
@@ -2,7 +2,8 @@ package com.NoName_hacke_.Artric;
 
 public class index {
     public static void main(String args) {
-        JDA jda = JDABuilder.createDefault("Nzg0MDEzMzQwMjUwNjAzNTMx.X8jHUg.kexqMQAk8rewPOVD2dlrX2Vh3pA").build();
+	:
+        JDA jda = JDABuilder.createDefault().build();
 
     }
 }
