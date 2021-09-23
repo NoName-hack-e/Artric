5c9d9206e80c9f00e21a73affba8975de7110b1b
NoName-hack-e <NoName_hacke@outlook.com>
2021-09-23 21:20:53 +0600

    HEKKK

--- src/com/NoName_hacke_/Artric/index.java
+++ src/com/NoName_hacke_/Artric/index.java
@@ -2,6 +2,7 @@ package com.NoName_hacke_.Artric;
 
 public class index {
     public static void main(String args) {
-        System.out.println("Testing helloo world");
+        JDA jda = JDABuilder.createDefault("Nzg0MDEzMzQwMjUwNjAzNTMx.X8jHUg.kexqMQAk8rewPOVD2dlrX2Vh3pA").build();
+
     }
 }
